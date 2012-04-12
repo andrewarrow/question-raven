d8e04de79cc9e63cd65ef557af052376fbb0dd3c
Andrew Arrow <andrew@anybeat.com>
2012-04-12 12:27:31 -0700

    show hello

--- client/question-raven.js
+++ client/question-raven.js
@@ -1,19 +1,7 @@
-if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to k.";
-  };
+Template.hello.show = function () {
+  return true;
+};
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
-}
-
-if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.dashboard.show = function () {
+  return false;
+};
