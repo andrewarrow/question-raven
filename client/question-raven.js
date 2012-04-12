5a04e4ade0d9e808d2ca6b4ca2fb7c0001e7666a
Andrew Arrow <andrew@anybeat.com>
2012-04-12 13:03:22 -0700

    logout

--- client/question-raven.js
+++ client/question-raven.js
@@ -6,8 +6,18 @@ Template.dashboard.show = function () {
   return Session.get('user_id') != null;
 };
 
+Template.right_nav.show = function () {
+  return Session.get('user_id') != null;
+};
+
 Template.hello.events = {
   'click #login' : function () {
-    Session.set('user_id', 1)
+    Session.set('user_id', 1);
+  }
+};
+
+Template.right_nav.events = {
+  'click #logout' : function () {
+    Session.set('user_id', null);
   }
 };
